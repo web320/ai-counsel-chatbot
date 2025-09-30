8947a5d9e43d93f47873c9fe4b0cbfc01d1c9dde
web320 <80985212+web320@users.noreply.github.com>
2025-09-30 22:54:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,8 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate(dict(st.secrets["firebase"]))
+   cred = credentials.Certificate(st.secrets["firebase"])
+
     firebase_admin.initialize_app(cred)
 db = firestore.client()
 
